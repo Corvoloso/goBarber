778398b679acb4d04e7a3b2df8b947c57fd5e0ce
corvoloso <igoralves.b18@gmail.com>
2020-06-25 10:46:30 -0300

    Mobile Structure Added

---
+++ mobile/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <View style={{ flex: 1, backgroundColor: '#312e38' }} />;
+};
+
+export default App;
