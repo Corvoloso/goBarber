464a3a1aaababde7a947aa6d0b07358a5f2b2a41
corvoloso <igoralves.b18@gmail.com>
2020-09-13 23:56:01 -0300

    Hooks Tests

---
+++ web/src/__test__/hooks/auth.spec.tsx
@@ -0,0 +1,54 @@
+import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
+import { useAuth, AuthProvider } from '../../hooks/auth';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
+describe('Auth hook', () => {
+  it('should be able to sign in', async () => {
+    const apiResponse = {
+      user: {
+        id: 'user-123',
+        name: 'John Doe',
+        email: 'johndoe@example.com.br',
+      },
+      token: 'token-123',
+    };
+
+    apiMock.onPost('/sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@example.com.br',
+      password: '123456',
+    });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:token',
+      apiResponse.token,
+    );
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(apiResponse.user),
+    );
+    expect(result.current.user.email).toEqual('johndoe@example.com.br');
+  });
+
+  // it('should be able to sign out', async () => {
+  //   const { result } = renderHook(() => useAuth(), {
+  //     wrapper: AuthProvider,
+  //   });
+
+  //   result.current.signOut();
+
+  //   expect(result.current.user).toEqual(undefined);
+  // });
+});
